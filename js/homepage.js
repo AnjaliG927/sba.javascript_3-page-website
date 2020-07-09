73a10990d70f2b9e599b7b27b274f203b4ae8474
AnjaliG927 <anjali.ghaligi@gmail.com>
2020-07-09 17:41:48 -0400

    second page

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,9 @@
 
 function validateName() {
-    let name = document.getElementById("username");
-    console.log(name);
-
-
+    let name = document.getElementById("username").value;
+    if (name == "") {
+        alert("Please enter your name");
+    } else {
+        document.getElementById("name").innerHTML = "Welcome! " + name;
+    }
 }
