83fdfe15bb2633d05c564a7622ee9d5f4c09ddc8
AnjaliG927 <anjali.ghaligi@gmail.com>
2020-07-10 16:21:13 -0400

    updated

--- js/createRecipe.js
+++ js/createRecipe.js
@@ -1,27 +1,40 @@
 
+function hideFunction() {
+    var hidelement = document.getElementById("hideMyelement");
+    if (hidelement.style.display === "none") {
+        hidelement.style.display = "block";
+    } else {
+        hidelement.style.display = "none";
+    }
+}
+
+
+
+
 let recipeName = document.getElementById("formGroupExampleInput").value;
 var arrHead = new Array();	// array for header.
 arrHead = ['', 'Ingredients', 'Measure'];
 
-let fileInput = document.getElementById('fileInput');
-fileInput.addEventListener('click', loadImageFileAsURL, false);
-console.log(fileInput);
 
-function loadImageFileAsURL() {
+function loadImageFileAsURL(event) {
 
-    if (fileInput.length > 0) {
-        let fileToLoad = filesSelected[0];
+
+    let selectedFile = event.target.files[0];
+    if (selectedFile) {
+
         let fileReader = new FileReader();
-        if (fileToLoad.type.match("*.png") || fileToLoad.type.match("*.jpeg")) {
-            fileReader.onload = function (fileLoadedEvent) {
-                let imageLoaded = document.createElement("img");
-                imageLoaded.src = fileLoadedEvent.target.result;
-                document.body.appendChild(imageLoaded);
-            };
-            fileReader.readAsDataURL(fileToLoad);
-            console.log(fileReader.readAsDataURL(fileToLoad));
-        }
+        var imgtag = document.getElementById("myimage");
+        imgtag.title = selectedFile.name;
+        fileReader.onload = function (event) {
+            imgtag.src = event.target.result;
+            imgtag.setAttribute("style", "float: left");
+
+        };
+        // console.log(fileReader.readAsDataURL(selectedFile));
+        fileReader.readAsDataURL(selectedFile);
     }
+
+
 }
 
 
@@ -30,6 +43,8 @@ function loadImageFileAsURL() {
 
 // first create TABLE structure with the headers. 
 function createTable() {
+    hideFunction(); //calling hide function onload event to hide div
+
     let ingredientTable = document.createElement('table');
     ingredientTable.setAttribute('id', 'ingredientTable');
     ingredientTable.setAttribute('class', 'table1'); // table id.
@@ -42,7 +57,9 @@ function createTable() {
     }
 
     let div = document.getElementById('cont');
+
     div.appendChild(ingredientTable);  // add the TABLE to the container.
+
 }
 function addTextBox() {
 
@@ -88,22 +105,55 @@ function removeRow(oButton) {
 
 // function to extract and submit table data.
 function submitButton() {
+    hideFunction();
+    displayName();
+
     let myTab = document.getElementById('ingredientTable');
-    let arrValues = new Array();
+    var arrValues = new Array();
 
     // loop through each row of the table.
     for (row = 1; row < myTab.rows.length - 1; row++) {
         // loop through each cell in a row.
         for (coln = 0; coln < myTab.rows[row].cells.length; coln++) {
             let element = myTab.rows.item(row).cells[coln];
+
             if (element.childNodes[0].getAttribute('type') == 'text') {
                 arrValues.push("'" + element.childNodes[0].value + "'");
+
             }
         }
+
+        displayTable(arrValues);
+        // The final output.
+        document.getElementById('output').innerHTML = arrValues;
+
+    }
+}
+function displayTable(arrValues) {
+    //Create a HTML Table element.
+
+    // --------------------------------------------------------------
+    var arrHead = new Array();	// array for header.
+    arrHead = ['', 'Ingredients', 'Measure'];
+    let ingredientTable = document.createElement('table');
+    ingredientTable.setAttribute('id', 'createDisplayTable');
+    ingredientTable.setAttribute('class', 'table table-bordered');
+
+    let tr = ingredientTable.insertRow(-1);
+    for (let h = 0; h < arrHead.length; h++) {
+        let th = document.createElement('th'); // create table headers
+        th.innerHTML = arrHead[h];
+        tr.appendChild(th);
     }
 
-    // The final output.
-    document.getElementById('output').innerHTML = arrValues;
-    // window.location.replace('cake.html?Param1=' + arrValues + '&Param2=' + document.getElementById('formGroupExampleInput').value);
-    //alert(window.location.href);
+    let div = document.getElementById('ingredients');
+
+    div.appendChild(ingredientTable);
+
+
+}
+
+function displayName() {
+    document.getElementById("displayname").innerHTML = document.getElementById("formGroupExampleInput").value;
+
 }
